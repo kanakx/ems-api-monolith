1eaf47a2f882c74a52a4c8f67515182d7ed87004
kanakx <kacperkanak@icloud.com>
2024-01-06 19:38:31 +0100

    Initial commit

---
+++ src/main/java/com/dev/emsapispring/EmsApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dev.emsapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsApiSpringApplication.class, args);
+    }
+
+}
