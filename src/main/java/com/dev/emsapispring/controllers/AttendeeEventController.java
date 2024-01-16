a0e1a130ce4ed98771a40d12ab6cc4aa7a698206
kanakx <kacperkanak@icloud.com>
2024-01-16 16:02:32 +0100

    Change path

--- src/main/java/com/dev/emsapispring/controllers/AttendeeEventController.java
+++ src/main/java/com/dev/emsapispring/controllers/AttendeeEventController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/attendeesEvents")
+@RequestMapping("/attendees-events")
 @RequiredArgsConstructor
 public class AttendeeEventController {
 
